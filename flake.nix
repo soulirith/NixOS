d9fb2969358575bb2fca3a009421c686320ed8d4
Edgars Rolmanis <edgars.7x@gmail.com>
2026-07-11 18:31:06 +0300

    Revert "rebuild: 2026-07-11 18:16"
    This reverts commit 54209990a2c83cd311a8aacbe79f881cdbda5cd8.

--- flake.nix
+++ flake.nix
@@ -2,7 +2,7 @@
   description = "My System Flake";
 
   inputs = {
-    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
     
     home-manager.url = "github:nix-community/home-manager";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
