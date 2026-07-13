df85aba4aa6317dbfa722cb446b394322db81bc8
Edgars Rolmanis <edgars.7x@gmail.com>
2026-07-13 10:02:04 +0300

    rebuild: 2026-07-13 10:02

--- home.nix
+++ home.nix
@@ -93,7 +93,22 @@ programs.zsh = {
       background_blur = "1";
      };
   };
-  
+
+  # Spotify
+  programs.spicetify = 
+    let
+      spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+    in {
+      enable = true;
+      theme = spicePkgs.themes.catppuccin;
+      colorScheme = "mocha";
+
+  enabledExtensions = with spicePkgs.extensions; [
+    adblock
+    hidePodcasts
+  ];
+};
+    
   # Cursor 
   home.pointerCursor = {
   enable = true;
