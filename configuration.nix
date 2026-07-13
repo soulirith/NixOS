f3b3b4fc59c0d10219235dc1e1877f17de22b9b7
Edgars Rolmanis <edgars.7x@gmail.com>
2026-07-13 09:58:32 +0300

    rebuild: 2026-07-13 09:58

--- configuration.nix
+++ configuration.nix
@@ -71,6 +71,32 @@
     };
   };
 
+  # Spotify
+  programs.spicetify =
+let
+  spicePkgs = inputs.spicetify-nix.legacyPackages.${pkgs.stdenv.hostPlatform.system};
+in
+{
+  enable = true;
+
+  enabledExtensions = with spicePkgs.extensions; [
+    adblock
+    hidePodcasts
+    shuffle # shuffle+ (special characters are sanitized out of extension names)
+  ];
+  enabledCustomApps = with spicePkgs.apps; [
+    newReleases
+    ncsVisualizer
+  ];
+  enabledSnippets = with spicePkgs.snippets; [
+    rotatingCoverart
+    pointer
+  ];
+
+  theme = spicePkgs.themes.catppuccin;
+  colorScheme = "mocha";
+}
+
   # NVIDIA Driver Settings
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.graphics.enable = true;
