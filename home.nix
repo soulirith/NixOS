01a10d8ae90624dc37acc203b225e2e7630d512c
Edgars Rolmanis <edgars.7x@gmail.com>
2026-07-14 00:43:58 +0300

    rebuild: 2026-07-14 00:43

--- home.nix
+++ home.nix
@@ -108,8 +108,10 @@
       confirm_os_window_close = 0;
       font_family = "JetBrainsMono Nerd Font";
       font_size = "10.0";
-      background_opacity = "0.5";
-      background_blur = "1";
+      background_opacity = "0.3";
+      background_blur = "0.5";
+      tab_bar_style = "powerline";
+      tab_powerline_style = "slanted";
     };
   };
 
