828a2be38472002fe883bc918c85acc5fb38da8c
Edgars Rolmanis <edgars.7x@gmail.com>
2026-07-16 12:33:09 +0300

    rebuild: 2026-07-16 12:33

--- configuration.nix
+++ configuration.nix
@@ -91,14 +91,16 @@
     };
   };
 
-  # NVIDIA Driver Settings
+  # NVIDIA
   services.xserver.videoDrivers = [ "nvidia" ];
   hardware.graphics.enable = true;
   hardware.nvidia = {
     package = config.boot.kernelPackages.nvidiaPackages.stable;
     modesetting.enable = true;
-    powerManagement.enable = true;
+    powerManagement = {
+    enable = true;
     finegrained = true;
+  };
     open = false;
     nvidiaSettings = true;
     prime = {
