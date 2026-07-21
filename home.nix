b66c33ec8d3f3e61a549236254db5197c3d7a093
Edgars Rolmanis <edgars.7x@gmail.com>
2026-07-21 22:45:56 +0300

    rebuild: 2026-07-21 22:45

--- home.nix
+++ home.nix
@@ -26,7 +26,11 @@
   };
 
   # Dark mode for nautilus
-  dconf.settings."org/gnome/desktop/interface".color-scheme = "prefer-dark";
+    dconf.settings."org/gnome/desktop/interface" = {
+    color-scheme = "prefer-dark";
+    cursor-theme = "catppuccin-mocha-dark-cursors";
+    cursor-size = 24;
+  };  
 
   # Cursor
   home.pointerCursor = {
