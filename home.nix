f61c76c24f06e8fef4138a9833c68e72a8c92019
Edgars Rolmanis <edgars.7x@gmail.com>
2026-07-21 22:52:13 +0300

    rebuild: 2026-07-21 22:52

--- home.nix
+++ home.nix
@@ -25,6 +25,16 @@
     };
   };
 
+   # libdecor reads the theme from here, fastfetch reads the cursor
+  xdg.configFile."gtk-3.0/settings.ini".text = ''
+    [Settings]
+    gtk-theme-name=Adwaita-dark
+    gtk-icon-theme-name=Adwaita
+    gtk-cursor-theme-name=catppuccin-mocha-dark-cursors
+    gtk-cursor-theme-size=24
+    gtk-application-prefer-dark-theme=1
+  '';
+
   # Dark mode for nautilus
     dconf.settings."org/gnome/desktop/interface" = {
     color-scheme = "prefer-dark";
