0125e7906196359bc01797cb8a10604b48a10ddb
Edgars Rolmanis <edgars.7x@gmail.com>
2026-07-26 01:39:40 +0300

    flake update: 2026-07-26 01:39

--- home.nix
+++ home.nix
@@ -92,29 +92,6 @@ programs.neovim = {
   defaultEditor = true;
   viAlias = true;
   vimAlias = true;
-  extraLuaConfig = ''
-    -- Bootstrap lazy.nvim
-    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
-    if not vim.loop.fs_stat(lazypath) then
-      vim.fn.system({
-        "git",
-        "clone",
-        "--filter=blob:none",
-        "https://github.com/folke/lazy.nvim.git",
-        "--branch=stable",
-        lazypath,
-      })
-    end
-    vim.opt.rtp:prepend(lazypath)
-
-    vim.opt.number = true
-    vim.opt.relativenumber = true
-    vim.opt.expandtab = true
-    vim.opt.shiftwidth = 2
-    vim.opt.tabstop = 2
-
-    require("lazy").setup("plugins")
-  '';
 };
 
 
