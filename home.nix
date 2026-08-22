51c084f444e6698c5918ce20213066e563c1f8d4
Edgars Rolmanis <edgars.7x@gmail.com>
2026-08-22 15:14:07 +0300

    rebuild: 2026-08-22 15:14

--- home.nix
+++ home.nix
@@ -77,20 +77,6 @@ programs.neovim = {
 })
 
 local function apply_custom_highlights()
-  local ok, base16 = pcall(require, 'base16-colorscheme')
-  if ok and base16.colorscheme then
-    local c = base16.colorscheme
-    vim.api.nvim_set_hl(0, 'Comment', { fg = c.base04, italic = true })
-    vim.api.nvim_set_hl(0, 'StatusLine', { fg = c.base05, bg = c.base01 })
-    vim.api.nvim_set_hl(0, 'StatusLineNC', { fg = c.base03, bg = c.base00 })
-  end
-end
-
-vim.opt.laststatus = 3
-
-require('matugen').setup()
-apply_custom_highlights()
-
 local signal = vim.uv.new_signal()
 signal:start('sigusr1', vim.schedule_wrap(function()
   apply_custom_highlights()
