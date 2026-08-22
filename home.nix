29136dae472a2c7858de727ca2effa118035e7ee
Edgars Rolmanis <edgars.7x@gmail.com>
2026-08-22 13:18:42 +0300

    rebuild: 2026-08-22 13:18

--- home.nix
+++ home.nix
@@ -47,21 +47,38 @@
   };
 
   # Neovim replaces nano
-programs.neovim = {
+  programs.neovim = {
   enable = true;
   defaultEditor = true;
   viAlias = true;
   vimAlias = true;
   extraLuaConfig = ''
+    -- Bootstrap lazy.nvim
+    local lazypath = vim.fn.stdpath("data") .. "/lazy/lazy.nvim"
+    if not vim.loop.fs_stat(lazypath) then
+      vim.fn.system({
+        "git", "clone", "--filter=blob:none",
+        "https://github.com/folke/lazy.nvim.git",
+        "--branch=stable", lazypath,
+      })
+    end
+    vim.opt.rtp:prepend(lazypath)
+
     vim.opt.number = true
     vim.opt.relativenumber = false
     vim.opt.expandtab = true
     vim.opt.shiftwidth = 2
     vim.opt.tabstop = 2
+
+    require("lazy").setup({
+      { "RRethy/base16-nvim" },
+    })
+
+    require('matugen').setup()
   '';
 };
 
-  # GTK 3.0
+ # GTK 3.0
   xdg.configFile."gtk-3.0/settings.ini".text = ''
     [Settings]
     gtk-theme-name=adw-gtk3-dark
