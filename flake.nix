f60ffad6eeae8d2e5b3fb0a3694fc49c56e27685
Edgars Rolmanis <edgars.7x@gmail.com>
2026-08-27 23:15:36 +0300

    rebuild: 2026-08-27 23:15

--- flake.nix
+++ flake.nix
@@ -1,22 +1,16 @@
 {
   description = "My System Flake";
-
-  # `follows` on every input keeps one nixpkgs in the lock instead of several
   inputs = {
     nixpkgs.url = "github:nixos/nixpkgs/nixos-26.05";
-   # nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     home-manager.url = "github:nix-community/home-manager/release-26.05";
     home-manager.inputs.nixpkgs.follows = "nixpkgs";
-
     noctalia.url = "github:noctalia-dev/noctalia/cachix";    
-
     noctalia-greeter.url = "github:noctalia-dev/noctalia-greeter";
     noctalia-greeter.inputs.nixpkgs.follows = "nixpkgs";
-
     spicetify-nix.url = "github:Gerg-L/spicetify-nix";
     spicetify-nix.inputs.nixpkgs.follows = "nixpkgs";
   };
-   outputs = { self, nixpkgs, home-manager, ... }@inputs: {
+  outputs = { self, nixpkgs, home-manager, ... }@inputs: {
     nixosConfigurations.nixos = nixpkgs.lib.nixosSystem {
       system = "x86_64-linux";
       specialArgs = { inherit inputs; };
@@ -32,13 +26,6 @@
         }
       ];
     };
-
-    homeConfigurations."soulirith" = home-manager.lib.homeManagerConfiguration {
-      pkgs = nixpkgs.legacyPackages.x86_64-linux;
-      extraSpecialArgs = { inherit inputs; };
-      modules = [ ./home.nix ];
-    };
   };
-
-    }
+}
 
